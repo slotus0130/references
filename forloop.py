dd6190c64f7e23778a21c3b60b39e3885ba463f4
slotus0130 <slotus0130@gmail.com>
2024-08-04 21:22:39 +0900

    Update forloop.py

--- forloop.py
+++ forloop.py
@@ -1,23 +1,13 @@
-# N = 7
-Nlist = [-1, 3, -2, -4, 1, -5, 3]
-
-sum_Nlist = sum(Nlist)
-previous = sum_Nlist
+Nlist = [1, 2, 3, 4, 5]
+# Nlist = [-1, 3, -2, -4, 1, -5, 3]
+totalsum = 0
+previous = sum(Nlist)
 
 for i in range(len(Nlist)):
     for j in range(i ,len(Nlist)):
-        sumV = 0
-        print(f"sublist : {Nlist[i:j+1]}")         
+        totalsum = sum(Nlist) + sum(Nlist[i:j+1]) * -2
+        print(f"totalsum : {totalsum}, previous : {previous}")
+        if totalsum > previous:
+            previous = totalsum 
 
-        for v in Nlist[i:j+1]:
-            v *= -1
-            sumV += v
-        
-        sumV = sumV * 2
-        total_sum = sum_Nlist + sumV
-
-        print(f"previous : {previous} , total_sum : {total_sum}")
-        if previous < total_sum:
-            previous = total_sum
-            
 print(previous)
